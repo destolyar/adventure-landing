c0b0fc93f97e6eeaadaa00cef76978b5e9b7a422
destolyar <dead.stolyar@gmail.com>
2022-03-03 00:22:44 +0300

    fech: finish subscribe section

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import '../styles/components/signup.scss'
 
 export const SignUp: React.FunctionComponent = () => {
   return(
-      <h1>SIGNUP</h1>
+      <section className="signup">
+        <div className="signup__background">
+          <img className="signup__background__first-cloud" src="/img/background-cloud-2.png" alt="" />
+          <img className="signup__background__second-cloud" src="/img/background-cloud-2.png" alt="" />
+          <img className="signup__background__third-cloud" src="/img/background-cloud-2.png" alt="" />
+        </div>
+        <div className="signup__content">
+          <h2 className="signup__content__title">Sign up to our newsletter!</h2>
+          <p className="signup__content__description">When you need to stay focused and alert on the right deals, our CRM is here to support you.</p>
+          <div className="signup__content__subscribe-form">
+            <input className="signup__content__subscribe-form__input" type="text" placeholder="email adress"/>
+            <button className="signup__content__subscribe-form__button">Subscribe</button>
+          </div>
+        </div>
+      </section>
   )
 }
